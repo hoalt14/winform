be4e64b2540ccbf5089d6ee7706aa0e34443484e
Hòa <lethanhhoa.boi@gmail.com>
2020-11-12 11:40:38 +0700

    update dental payment app

---
+++ DentalPaymentApp/frmDentalPaymentApp.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace DentalPaymentApp
+{
+    partial class frmDentalPaymentApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblFilling = new System.Windows.Forms.Label();
+            this.lblCleanCost = new System.Windows.Forms.Label();
+            this.lblWhiteningCost = new System.Windows.Forms.Label();
+            this.lblXRayCost = new System.Windows.Forms.Label();
+            this.lblFillingCost = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.chkClean = new System.Windows.Forms.CheckBox();
+            this.chkWhitening = new System.Windows.Forms.CheckBox();
+            this.chkXRay = new System.Windows.Forms.CheckBox();
+            this.nUPFilling = new System.Windows.Forms.NumericUpDown();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnTinhTien = new System.Windows.Forms.Button();
+            this.lbxLog = new System.Windows.Forms.ListBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nUPFilling)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Blue;
+            this.lblTitle.Location = new System.Drawing.Point(97, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(173, 20);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Dental Payment App";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(31, 45);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(86, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Tên khách hàng";
+            // 
+            // lblFilling
+            // 
+            this.lblFilling.AutoSize = true;
+            this.lblFilling.Location = new System.Drawing.Point(52, 215);
+            this.lblFilling.Name = "lblFilling";
+            this.lblFilling.Size = new System.Drawing.Size(55, 13);
+            this.lblFilling.TabIndex = 5;
+            this.lblFilling.Text = "Trám răng";
+            // 
+            // lblCleanCost
+            // 
+            this.lblCleanCost.AutoSize = true;
+            this.lblCleanCost.Location = new System.Drawing.Point(253, 95);
+            this.lblCleanCost.Name = "lblCleanCost";
+            this.lblCleanCost.Size = new System.Drawing.Size(52, 13);
+            this.lblCleanCost.TabIndex = 6;
+            this.lblCleanCost.Text = "$100.000";
+            // 
+            // lblWhiteningCost
+            // 
+            this.lblWhiteningCost.AutoSize = true;
+            this.lblWhiteningCost.Location = new System.Drawing.Point(244, 134);
+            this.lblWhiteningCost.Name = "lblWhiteningCost";
+            this.lblWhiteningCost.Size = new System.Drawing.Size(61, 13);
+            this.lblWhiteningCost.TabIndex = 7;
+            this.lblWhiteningCost.Text = "$1.200.000";
+            // 
+            // lblXRayCost
+            // 
+            this.lblXRayCost.AutoSize = true;
+            this.lblXRayCost.Location = new System.Drawing.Point(253, 176);
+            this.lblXRayCost.Name = "lblXRayCost";
+            this.lblXRayCost.Size = new System.Drawing.Size(52, 13);
+            this.lblXRayCost.TabIndex = 8;
+            this.lblXRayCost.Text = "$200.000";
+            // 
+            // lblFillingCost
+            // 
+            this.lblFillingCost.AutoSize = true;
+            this.lblFillingCost.Location = new System.Drawing.Point(240, 215);
+            this.lblFillingCost.Name = "lblFillingCost";
+            this.lblFillingCost.Size = new System.Drawing.Size(65, 13);
+            this.lblFillingCost.TabIndex = 9;
+            this.lblFillingCost.Text = "$80.000/cái";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(161, 275);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(31, 13);
+            this.lblTotal.TabIndex = 10;
+            this.lblTotal.Text = "Total";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(138, 45);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(167, 20);
+            this.txtName.TabIndex = 11;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Enabled = false;
+            this.txtTotal.Location = new System.Drawing.Point(205, 268);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 12;
+            // 
+            // chkClean
+            // 
+            this.chkClean.AutoSize = true;
+            this.chkClean.Location = new System.Drawing.Point(55, 95);
+            this.chkClean.Name = "chkClean";
+            this.chkClean.Size = new System.Drawing.Size(62, 17);
+            this.chkClean.TabIndex = 13;
+            this.chkClean.Text = "Cạo vôi";
+            this.chkClean.UseVisualStyleBackColor = true;
+            // 
+            // chkWhitening
+            // 
+            this.chkWhitening.AutoSize = true;
+            this.chkWhitening.Location = new System.Drawing.Point(55, 130);
+            this.chkWhitening.Name = "chkWhitening";
+            this.chkWhitening.Size = new System.Drawing.Size(71, 17);
+            this.chkWhitening.TabIndex = 14;
+            this.chkWhitening.Text = "Tẩy trắng";
+            this.chkWhitening.UseVisualStyleBackColor = true;
+            // 
+            // chkXRay
+            // 
+            this.chkXRay.AutoSize = true;
+            this.chkXRay.Location = new System.Drawing.Point(55, 172);
+            this.chkXRay.Name = "chkXRay";
+            this.chkXRay.Size = new System.Drawing.Size(98, 17);
+            this.chkXRay.TabIndex = 15;
+            this.chkXRay.Text = "Chụp hình răng";
+            this.chkXRay.UseVisualStyleBackColor = true;
+            // 
+            // nUPFilling
+            // 
+            this.nUPFilling.Location = new System.Drawing.Point(113, 208);
+            this.nUPFilling.Name = "nUPFilling";
+            this.nUPFilling.Size = new System.Drawing.Size(79, 20);
+            this.nUPFilling.TabIndex = 16;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(72, 311);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 17;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnTinhTien
+            // 
+            this.btnTinhTien.Location = new System.Drawing.Point(195, 311);
+            this.btnTinhTien.Name = "btnTinhTien";
+            this.btnTinhTien.Size = new System.Drawing.Size(75, 23);
+            this.btnTinhTien.TabIndex = 18;
+            this.btnTinhTien.Text = "Tính tiền";
+            this.btnTinhTien.UseVisualStyleBackColor = true;
+            this.btnTinhTien.Click += new System.EventHandler(this.btnTinhTien_Click);
+            // 
+            // lbxLog
+            // 
+            this.lbxLog.FormattingEnabled = true;
+            this.lbxLog.Location = new System.Drawing.Point(336, 45);
+            this.lbxLog.Name = "lbxLog";
+            this.lbxLog.Size = new System.Drawing.Size(247, 199);
+            this.lbxLog.TabIndex = 19;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(372, 270);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 20;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(464, 270);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 21;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // frmDentalPaymentApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 361);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lbxLog);
+            this.Controls.Add(this.btnTinhTien);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.nUPFilling);
+            this.Controls.Add(this.chkXRay);
+            this.Controls.Add(this.chkWhitening);
+            this.Controls.Add(this.chkClean);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblFillingCost);
+            this.Controls.Add(this.lblXRayCost);
+            this.Controls.Add(this.lblWhiteningCost);
+            this.Controls.Add(this.lblCleanCost);
+            this.Controls.Add(this.lblFilling);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "frmDentalPaymentApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dental Payment Application";
+            ((System.ComponentModel.ISupportInitialize)(this.nUPFilling)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblFilling;
+        private System.Windows.Forms.Label lblCleanCost;
+        private System.Windows.Forms.Label lblWhiteningCost;
+        private System.Windows.Forms.Label lblXRayCost;
+        private System.Windows.Forms.Label lblFillingCost;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.CheckBox chkClean;
+        private System.Windows.Forms.CheckBox chkWhitening;
+        private System.Windows.Forms.CheckBox chkXRay;
+        private System.Windows.Forms.NumericUpDown nUPFilling;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnTinhTien;
+        private System.Windows.Forms.ListBox lbxLog;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ DentalPaymentApp/frmDentalPaymentApp.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DentalPaymentApp
+{
+    public partial class frmDentalPaymentApp : Form
+    {
+        public frmDentalPaymentApp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnTinhTien_Click(object sender, EventArgs e)
+        {
+            if(txtName.Text == "")
+            {
+                MessageBox.Show("Chưa nhập tên khách hàng!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                float sum = 0;
+
+                if (chkClean.Checked)
+                {
+                    sum += 100000;
+                }
+
+                if (chkWhitening.Checked)
+                {
+                    sum += 1200000;
+                }
+
+                if (chkXRay.Checked)
+                {
+                    sum += 200000;
+                }
+
+                float count = Convert.ToInt32(nUPFilling.Value);
+                sum += count * 80000;
+
+                txtTotal.Text = "$" + sum.ToString();
+                lbxLog.Items.Add(txtName.Text + " - " + txtTotal.Text);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("C:/Users/boi/Desktop/test/log.txt", FileMode.Create);
+            StreamWriter wr = new StreamWriter(fs, Encoding.UTF8);
+            foreach (string velo in lbxLog.Items)
+            {
+                wr.WriteLine(velo);
+            }
+            wr.Flush();
+            fs.Close();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("C:/Users/boi/Desktop/test/log.txt", FileMode.Open);
+            StreamReader rd = new StreamReader(fs, Encoding.UTF8);
+            string velo = rd.ReadToEnd();
+            lbxLog.Items.Add(velo);
+            rd.Close();
+        }
+    }
+}
